741bd706d46ad5cc964ef9d23543dd976a53cfbb
Bouke Haarsma <bouke@haarsma.eu>
2017-04-05 07:39:01 +0200

    Removed unused code

--- Sources/DNS/Data+Extensions.swift
+++ Sources/DNS/Data+Extensions.swift
@@ -1,19 +1,5 @@
 import Foundation
 
-// Removed in Xcode 8 beta 3
-func + (lhs: Data, rhs: Data) -> Data {
-    var result = lhs
-    result.append(rhs)
-    return result
-}
-
-// Removed in Xcode 8 beta 3
-extension Data {
-    init<C: Collection>(_ collection: C) where C.Iterator.Element == UInt8 {
-        self = Data(bytes: Array(collection))
-    }
-}
-
 extension Data {
     init?(hex: String) {
         var result = [UInt8]()
@@ -45,7 +31,6 @@ extension RandomAccessCollection where Iterator.Element == UInt8, Index == Int {
         while start < endIndex {
             _ = formIndex(&end, offsetBy: 24, limitedBy: endIndex)
 
-//            print(UInt32(distance(from: startIndex, to: start).toIntMax()).bytes.hex, terminator: ":  ")
             print(String(format: "%6d:  ", UInt32(distance(from: startIndex, to: start).toIntMax())), terminator: "")
 
             var byteStart = start
