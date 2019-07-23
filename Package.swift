21a56fc9ff4c65d1ff3931a5d9479c556f3ead64
twodayslate <zac@gorak.us>
2019-07-23 11:03:07 -0700

    Updated Swift Package to 4.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
@@ -11,5 +11,5 @@ let package = Package(
         .target(name: "DNS", dependencies: []),
         .testTarget(name: "DNSTests", dependencies: ["DNS"])
     ],
-    swiftLanguageVersions: [4]
+    swiftLanguageVersions: [.v4_2]
 )
